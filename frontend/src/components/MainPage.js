c40b2c8f9482f9a70cdef3334e4864c404edf933
Shiyu Xiu <xiushiyu666@gmail.com>
2021-10-02 14:39:34 -0600

    finish header

---
+++ frontend/src/components/MainPage.js
@@ -0,0 +1,9 @@
+function MainPage() {
+  return (
+    <div className="MainPage">
+      <h1>let us connect with your friends</h1>
+    </div>
+  );
+}
+
+export default MainPage;
---
+++ frontend/src/components/Profile.js
@@ -0,0 +1,5 @@
+function Profile() {
+  return <div className="Profile"></div>;
+}
+
+export default Profile;
