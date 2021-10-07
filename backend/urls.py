71811a756697bdc21c3ccf67577a42fe7c39b3ea
xichen1 <xichen.pan@gmail.com>
2021-10-07 08:42:10 -0400

    create user app, enable getting, creating users

--- backend/urls.py
+++ backend/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from users import views
+
 
 urlpatterns = [
+    path('users/', views.user_list, name='user_list'),
+    path('users/<uuid:id>', views.user_detail, name='user_detail'),
     path('admin/', admin.site.urls),
 ]
---
+++ users/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from .models import User
+from django.db.models import fields
+
+
+class UserSerializer(serializers.ModelSerializer):
+    id = serializers.UUIDField()
+    user_name = serializers.CharField(required=True)
+    github_name = serializers.CharField(required=True)
+
+    def create(self, validated_data):
+        """
+        Create and return a new `Snippet` instance, given the validated data.
+        """
+        return User.objects.create(**validated_data)
+
+    class Meta:
+        model = User
+        fields = ['id', 'user_name', 'github_name']
