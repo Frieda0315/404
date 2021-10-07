41d9582cd3c9a4fdc5d634e9fc466eea474da26e
ZjTan4 <ztan4@ualberta.ca>
2021-10-07 10:05:28 -0600

    something seems wrong, don't know what happened yet

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,7 +1,11 @@
+
+import PostStream from "./PostStream";
+
 function MainPage() {
   return (
     <div className="MainPage">
       <h1>let us connect with your friends</h1>
+      <h2>{PostStream()}</h2>
     </div>
   );
 }
