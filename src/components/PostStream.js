a175875d837bddc65eaaa36b0a681183594abf7e
ZjTan4 <ztan4@ualberta.ca>
2021-10-07 16:17:35 -0600

    fix some bugs, now it's more elegant

--- src/components/PostStream.js
+++ src/components/PostStream.js
@@ -1,10 +1,10 @@
 
 import Grid from "@material-ui/core/Grid";
-//import Item from "@material-ui/core/ListItem"
+import { Avatar } from "@material-ui/core";
 import { Card } from "reactstrap";
 import makeStyles from "@material-ui/styles/makeStyles";
 import React from "react";
-import { Box, CardMedia, CardActionArea, CardContent, Divider, Typography } from "@material-ui/core";
+import { CardMedia, CardActionArea, CardContent, Typography } from "@material-ui/core";
 import dummy_image from "../static/musle.png"
 
 const useStyles = makeStyles(() =>({
@@ -13,6 +13,9 @@ const useStyles = makeStyles(() =>({
     }, 
     postBody: {
         marginLeft: "100px"
+    },
+    postImage: {
+        width: "75px"
     }
 }));
 
@@ -31,36 +34,46 @@ function PostStream() {
     const tempPostOnClick = (ev) => {
         console.log("clicked a post");
     };
+    const tempSummary = "poster body summary";
     const postStream = postList.map((post) => 
         <Grid item xs={8} className={styleClasses.postBody}>
-            <Typography  variant="subtitle1" color="text.secondary" component="div"> 
-                Xiu666
-            </Typography>
-            <Typography  variant="subtitle1" color="text.secondary" component="div"> 
-                Date: xxxx-xx-xx xx:xx
-            </Typography>
-            <Card sx={{ display: 'flex' }}>
-                <CardMedia
-                    component="img"
-                    sx={{ width: 151 }}
-                    image={dummy_image}
-                    alt="Live from space album cover"
-                />
-                <CardActionArea onClick={tempPostOnClick}>
-                    <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-                    <CardContent sx={{ flex: '1 0 auto' }}>
-                        <Typography component="div" variant="h5">
-                        {post}
-                        </Typography>
-                        <Typography variant="subtitle1" color="text.secondary" component="div">
-                        poster body summary
-                        </Typography>
-                    </CardContent>
-                    </Box> 
-                </CardActionArea>
-                
+            <Grid container>
+                <Grid item xs={1}>
+                    <Avatar src={dummy_image}></Avatar>
+                </Grid>
+                <Grid item xs={4}>
+                    <Typography  variant="subtitle1" color="text.secondary" component="div"> 
+                        Xiu666
+                    </Typography>
+                    <Typography  variant="subtitle1" color="text.secondary" component="div"> 
+                        Date: xxxx-xx-xx xx:xx
+                    </Typography>
+                </Grid>
+            </Grid>
+            <Card>
+                <Grid container>
+                    <Grid item xs={2}>
+                        <CardMedia
+                            component="img"
+                            className={styleClasses.postImage}
+                            image={dummy_image}
+                            alt="dummy"
+                        />
+                    </Grid>
+                    <Grid item xs={10}>
+                        <CardActionArea onClick={tempPostOnClick}>
+                            <CardContent sx={{ flex: '1 0 auto' }}>
+                                <Typography component="div" variant="h5">
+                                    {post}
+                                </Typography>
+                                <Typography variant="subtitle1" color="text.secondary" component="div">
+                                    {tempSummary}
+                                </Typography>
+                            </CardContent> 
+                        </CardActionArea>   
+                    </Grid>
+                </Grid>
             </Card>
-            <Divider/>
         </Grid>
         
     );
