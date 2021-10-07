a2f1ff95ca045e1294b668f7dafead070840410a
ZjTan4 <ztan4@ualberta.ca>
2021-10-07 11:56:03 -0600

    update the post stream items, still ugly though

--- src/components/PostStream.js
+++ src/components/PostStream.js
@@ -4,10 +4,19 @@ import Grid from "@material-ui/core/Grid";
 import { Card } from "reactstrap";
 import makeStyles from "@material-ui/styles/makeStyles";
 import React from "react";
-const useStyles = makeStyles(() =>({
+import { Box, CardMedia, CardActionArea, CardContent, Divider, Typography } from "@material-ui/core";
+import dummy_image from "../static/musle.png"
 
+const useStyles = makeStyles(() =>({
+    stream: {
+        marginLeft: "10px"
+    }, 
+    postBody: {
+        marginLeft: "100px"
+    }
 }));
 
+// dummy val
 const postList = [
     "Hello world", 
     "I am just trying to make a stream", 
@@ -18,13 +27,45 @@ const postList = [
 ]
 
 function PostStream() {
+    const styleClasses = useStyles();
+    const tempPostOnClick = (ev) => {
+        console.log("clicked a post");
+    };
     const postStream = postList.map((post) => 
-    <Grid item xs={8}>
-        <Card> {post} </Card>
-    </Grid>);
-
+        <Grid item xs={8} className={styleClasses.postBody}>
+            <Typography  variant="subtitle1" color="text.secondary" component="div"> 
+                Xiu666
+            </Typography>
+            <Typography  variant="subtitle1" color="text.secondary" component="div"> 
+                Date: xxxx-xx-xx xx:xx
+            </Typography>
+            <Card sx={{ display: 'flex' }}>
+                <CardMedia
+                    component="img"
+                    sx={{ width: 151 }}
+                    image={dummy_image}
+                    alt="Live from space album cover"
+                />
+                <CardActionArea onClick={tempPostOnClick}>
+                    <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                    <CardContent sx={{ flex: '1 0 auto' }}>
+                        <Typography component="div" variant="h5">
+                        {post}
+                        </Typography>
+                        <Typography variant="subtitle1" color="text.secondary" component="div">
+                        poster body summary
+                        </Typography>
+                    </CardContent>
+                    </Box> 
+                </CardActionArea>
+                
+            </Card>
+            <Divider/>
+        </Grid>
+        
+    );
     return (
-        <Grid container spacing={2}>
+        <Grid container spacing={2} className={styleClasses.stream}>
             {postStream}
         </Grid>
     )
