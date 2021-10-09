f07fa521bd0cfe45afa37416d7b6649132594919
frieda0315 <frieda1@ualberta.ca>
2021-10-09 12:22:01 -0600

    Post

--- src/App.js
+++ src/App.js
@@ -2,8 +2,15 @@ import { Layout } from "./components/Layout";
 import MainPage from "./components/MainPage";
 import { Route } from "react-router-dom";
 import Profile from "./components/Profile";
+import Post from "./components/Post"
+import Button from '@mui/material/Button';
+import { useHistory } from "react-router-dom"
+import Box from '@mui/material/Box';
+
 
 function App() {
+  const history = useHistory();
+
   return (
     <header>
       <Layout>
@@ -13,9 +20,29 @@ function App() {
         <Route exact path="/profile/">
           <Profile></Profile>
         </Route>
+        <Route exact path="/new/">
+          <Post></Post>
+        </Route>
+        <Box
+            sx={{p: 1,    bgcolor:  '#e0e0e0'}}>
+
+              <Button 
+              sx={{
+                p: 1,    
+                textAlign: 'end'
+                }}
+              variant="contained" 
+               onClick={() => {
+                        let path = `/new/`; 
+                  history.push(path);
+                  }}>
+                New
+              </Button>
+        </Box>
       </Layout>
     </header>
   );
 }
 
 export default App;
+
