2ee804820cf9f3c016a55ca2eff75d58431abaad
Bingshen Yang <bi2@ualberta.ca>
2021-10-24 13:18:19 +0800

    fix author Foreign key bug and some useless import

--- posts/models.py
+++ posts/models.py
@@ -7,23 +7,22 @@ from users.models import User
 
 class Post(models.Model):
     id = models.UUIDField(
-    primary_key=True, default=uuid.uuid4, editable=False, unique=True)
+        primary_key=True, default=uuid.uuid4, editable=False, unique=True)
     title = models.CharField(max_length=120)
     content = models.TextField()
-    published = models.DateTimeField(auto_now_add=True,null=True)
+    published = models.DateTimeField(auto_now_add=True, null=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
-    #image type
-    #TODO: image field
+    # image type
+    # TODO: image field
     image = models.ImageField(blank=True, upload_to='img')
 
-    #the first column is the actual value in databse, the second column is the human-readable value in admin selecting page
+    # the first column is the actual value in databse, the second column is the human-readable value in admin selecting page
     visibilityTypes = [("PUBLIC",     "Public"),
-                         #("FOAF",       "Friends of Friends"),
-                         ("FRIENDS",    "Friends"),
-                         ("PRIVATE",    "Private"),
-                         ("SERVERONLY", "Local Frchiends")]
-    visibility = models.CharField(max_length=30, choices=visibilityTypes, default="PUBLIC")
-
+                       #("FOAF",       "Friends of Friends"),
+                       ("FRIENDS",    "Friends"),
+                       ("PRIVATE",    "Private"),
+                       ("SERVERONLY", "Local Frchiends")]
+    visibility = models.CharField(
+        max_length=30, choices=visibilityTypes, default="PUBLIC")
     # class Meta:
     #     db_table = "post"
-
--- posts/post_views.py
+++ posts/post_views.py
@@ -5,8 +5,7 @@ from rest_framework.response import Response
 from rest_framework.parsers import JSONParser
 from .serializers import PostSerializer
 from .models import Post
-from django.core.files.base import ContentFile
-from django.core.files.storage import FileSystemStorage
+
 
 # Create your views here.
 
@@ -14,15 +13,15 @@ from django.core.files.storage import FileSystemStorage
 @api_view(['GET', 'POST'])
 def post_list(request, author_id):
     if request.method == 'GET':
-        
-        #retrieve all public posts
+
+        # retrieve all public posts
         posts = Post.objects.filter(visibility="PUBLIC")
-        #retrieve all posts of this author， this might be a bug
+        # retrieve all posts of this author， this might be a bug
         #posts = posts.filter(author_id=author_id)
         serializer = PostSerializer(posts, many=True)
         return JsonResponse(serializer.data, safe=False)
     elif request.method == 'POST':
-        #get POST JSON Object
+        # get POST JSON Object
         json_data = JSONParser().parse(request)
         serializer = PostSerializer(data=json_data)
         #
@@ -32,9 +31,8 @@ def post_list(request, author_id):
         return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-
-@api_view(['GET', 'PUT', 'DELETE','PUT'])
-def post_detail(request, author_id, id): #this id here is postID
+@api_view(['GET', 'PUT', 'DELETE', 'PUT'])
+def post_detail(request, author_id, id):  # this id here is postID
     """
     Retrieve, update or delete a code snippet.
     """
@@ -48,48 +46,40 @@ def post_detail(request, author_id, id): #this id here is postID
         return JsonResponse(serializer.data)
 
     elif request.method == 'PUT':
-        #use serilizer to change post instance data to current request data
+        # use serilizer to change post instance data to current request data
         serializer = PostSerializer(post, data=request.data)
-        #if you are modifying your own posts
+        # if you are modifying your own posts
         if(post.author.id == author_id):
             if serializer.is_valid():
                 serializer.save()
                 return Response(serializer.data)
-        #modifying others posts is not allowed
+        # modifying others posts is not allowed
         else:
-            return Response(status.HTTP_400_BAD_REQUEST)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response(status.HTTP_403_FORBIDDEN)
+        # return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     elif request.method == 'DELETE':
-        #posts = Post.objects.filter(author_id=)
-        #print(post._meta.get_field("published"))
-        #print(request.user.id)
-        #print(post.author.id)
+        # posts = Post.objects.filter(author_id=)
+        # print(post._meta.get_field("published"))
+        # print(request.user.id)
+        # print(post.author.id)
         post = Post.objects.get(pk=id)
-        #if current post is owned by this author, then delete
+        # if current post is owned by this author, then delete
         if(post.author.id == author_id):
             post.delete()
             return Response(status=status.HTTP_204_NO_CONTENT)
         else:
-            #you cannot delete other arthor's post
-            return Response(status=status.HTTP_404_NOT_FOUND)
-        
-        
-        
-    
+            # you cannot delete other arthor's post
+            return Response(status=status.HTTP_403_FORBIDDEN)
 
-    #TODO
-    #elif request.method == 'POST':
-
-
-
+    # TODO
+    # elif request.method == 'POST':
 
 
 # helper function under
 
 
 # def imageHelper(request):
-#     file_content = ContentFile(request.FILES['img'].read())  
-#     img = Post(name = request.FILES['img'].name, img = request.FILES['img'])  
+#     file_content = ContentFile(request.FILES['img'].read())
+#     img = Post(name = request.FILES['img'].name, img = request.FILES['img'])
 #     img.save()
-
