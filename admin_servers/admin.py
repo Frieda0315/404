04e90348692584f0207f80ad280f2c0b20357c53
shiyuxiu <xiushiyu666@gmail.com>
2021-11-09 21:02:52 -0700

    finish node

--- admin_servers/admin.py
+++ admin_servers/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
 from .models import Node
 # Register your models here.
-admin.register(Node)
+admin.site.register(Node)
--- admin_servers/models.py
+++ admin_servers/models.py
@@ -1,12 +1,10 @@
 from django.db import models
-from .models import User
-import uuid
 
 # Create your models here.
 
 
 class Node(models.Model):
-    url = models.CharField(primary_key=True,unique=True)
+    url = models.CharField(primary_key=True,unique=True,max_length=180)
     user_name = models.CharField(max_length=120)
     password = models.CharField(max_length=120)
 
