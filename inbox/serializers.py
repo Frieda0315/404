cbf128856dfef88e50599c0a646d8b9c3ae37674
ZjTan4 <ztan4@ualberta.ca>
2021-11-20 09:46:22 -0700

    revert inbox changes

--- inbox/serializers.py
+++ inbox/serializers.py
@@ -3,15 +3,13 @@ from rest_framework import serializers
 from .models import Inbox
 from posts.serializers import PostSerializer
 from users.serializers import UserSerializer
-from likes.serializers import LikeSerializer
+
 from users.models import User
-from likes.models import Like
 from posts.models import Post
 
 
 class InboxSerializer(serializers.ModelSerializer):
-    post = PostSerializer(many=True, required=False)
-    like = LikeSerializer(many=True, required=False)
+    post = PostSerializer(many=True)
     receive_author = UserSerializer()
 
     def create(self, validated_data):
@@ -21,18 +19,18 @@ class InboxSerializer(serializers.ModelSerializer):
         instance = Inbox.objects.get_or_create(
             receive_author=author)[0]
 
-        if validated_data.get('post', None) != None:
-            post_data = validated_data.pop("post")
-            post = Post.objects.get(pk=post_data[0]["id"])
-            instance.post.add(post)
-        elif validated_data.get('like', None) != None:
-            #print(validated_data)
-            like_data = validated_data.pop("like")
-            like = Like.objects.get(pk=like_data[0]["id"])
-            instance.like.add(like)
-        # post_data = validated_data.pop("post", None)
-        # post = Post.objects.get(pk=post_data[0]["id"])
-        # instance.post.add(post)
+        # if validated_data.get('post', None) != None:
+        #     post_data = validated_data.pop("post")
+        #     post = Post.objects.get(pk=post_data[0]["id"])
+        #     instance.post.add(post)
+        # elif validated_data.get('like', None) != None:
+        #     #print(validated_data)
+        #     like_data = validated_data.pop("like")
+        #     like = Like.objects.get(pk=like_data[0]["id"])
+        #     instance.like.add(like)
+        post_data = validated_data.pop("post", None)
+        post = Post.objects.get(pk=post_data[0]["id"])
+        instance.post.add(post)
 
         # like_data = validated_data.pop("like", None)
         # like = Like.objects.get(pk=like_data[0]["id"])
@@ -42,7 +40,8 @@ class InboxSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Inbox
-        fields = ['like', 'post', 'receive_author']
+        fields = ['post', 'receive_author']
+        #fields = ['like', 'post', 'receive_author']
         # extra_kwargs = {
         #     'like': {'required': False}, 
         #     'post': {'required': False}, 
