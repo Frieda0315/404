49823868019ede321e025a8cafbfc269dc57b0a6
XichenPan <xichen.pan@gmail.com>
2021-11-24 16:33:58 -0700

    Remove session auth

--- backend/settings.py
+++ backend/settings.py
@@ -61,7 +61,6 @@ INSTALLED_APPS = [
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
     ]
 }
 
