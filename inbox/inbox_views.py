c7d48aba04db61d5c8b255caa07be23e6ced1acd
xichen1 <xichen.pan@gmail.com>
2021-12-02 21:44:20 -0700

    inbox comment api

--- inbox/inbox_views.py
+++ inbox/inbox_views.py
@@ -1,8 +1,3 @@
-
-from django.db.models.base import Model
-
-import json
-
 from django.http.response import JsonResponse
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from rest_framework.parsers import JSONParser
@@ -19,6 +14,8 @@ from follows.models import Follow, FriendRequest
 from follows.serializers import FriendRequestSerializer
 from posts.models import Post
 from posts.serializers import PostSerializer
+from comments.models import Comments
+from comments.serializers import CommentsSerializer, CommentSerializer
 from backend.helper import *
 from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
@@ -76,6 +73,49 @@ def handlePostRequest(json_data, receiver):
     inbox.post.add(post)
     inbox.save()
     return JsonResponse({"result": "Post shared"}, status=status.HTTP_200_OK, safe=False)
+
+
+def handleCommentRequest(json_data, receiver):
+    commenter_object = json_data["author"]
+    try:
+        commenter = User.objects.get(id=commenter_object["id"])
+    except User.DoesNotExist:
+        commenter_serializer = UserSerializer(data=commenter_object)
+        if commenter_serializer.is_valid():
+            commenter_serializer.save()
+            commenter = commenter_serializer.data
+        else:
+            return JsonResponse(commenter_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    post_id = json_data["id"].split("posts/")[1]
+    post_id = post_id.split("/comments")[0]
+
+    author_id = json_data["id"].split("author/")[1]
+    author_id = post_id.split("/posts")[0]
+
+    query_string = "author/"+str(author_id)+"/posts/"+str(post_id)
+    try:
+        post = Post.objects.get(uuid=post_id)
+    except Post.DoesNotExist:
+        return JsonResponse({"error": "comment not found"}, status=status.HTTP_404_NOT_FOUND)
+    comment_lists = Comments.objects.filter(
+        post__contains=query_string)
+    if not comment_lists:
+        return JsonResponse({"error": "comment not found"}, status=status.HTTP_404_NOT_FOUND)
+    comment_list = comment_lists[0]
+    uuid_data = comment_id_parser(json_data)
+    serializer = CommentSerializer(data=json_data)
+    if serializer.is_valid():
+        new_comment = serializer.save()
+        new_comment.uuid = uuid_data
+        new_comment.save()
+        # Reference: https://docs.djangoproject.com/en/3.2/topics/db/examples/many_to_many/#many-to-many-relationships
+        comment_list.comments.add(new_comment)
+        comment_list.save()
+        post.count += 1
+        post.save()
+
+        return JsonResponse(serializer.data, status=status.HTTP_201_CREATED)
+    return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 def handleLikeRequest(json_data, receiver):
@@ -147,6 +187,8 @@ def inbox_list(request, author_id):
             return handlePostRequest(json_data, receiver)
         elif json_data["type"] == "Like":
             return handleLikeRequest(json_data, receiver)
+        elif json_data["type"] == "comment":
+            return handleCommentRequest(json_data, receiver)
     # Get shared posts
     elif request.method == 'GET':
         try:
